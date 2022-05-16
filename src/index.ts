0bebfee0ea71e1a3552ebd4257e6c1d6e7956418
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-16 14:34:38 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function calcularÁreaTriángulo(base: number, altura: number): number {
+  let resultado: number = (base * altura) / 2;
+  return resultado;
+}
+
+console.log("El área del triángulo es: " + calcularÁreaTriángulo(1, 2));
+console.log("El área del triángulo es: " + calcularÁreaTriángulo(2, 4));
+console.log("El área del triángulo es: " + calcularÁreaTriángulo(3, 6));
+console.log("El área del triángulo es: " + calcularÁreaTriángulo(4, 8));
+console.log("El área del triángulo es: " + calcularÁreaTriángulo(5, 10));
+console.log("El área del triángulo es: " + calcularÁreaTriángulo(6, 12));
+console.log("El área del triángulo es: " + calcularÁreaTriángulo(7, 14));
